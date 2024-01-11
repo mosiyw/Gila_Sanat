275c2ccb526943141bd6a8748f819118d6c68f87
mosiyw <amirpc.a@yahoo.com>
2024-01-11 20:11:33 +0330

    Usermodal changed

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,9 +4,16 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   username: String,
   email: String,
+  firstname: String,
+  lastname: String,
   phone_number: String,
   password: String,
   isAdmin: { type: Boolean, default: false },
+  addresses: [],
+  favorite: [],
+  cart: [],
+  orders: [],
+  tickets: [],
 });
 
 module.exports = mongoose.model("User", userSchema);
