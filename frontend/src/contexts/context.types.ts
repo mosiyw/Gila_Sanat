a0bfd20e401b2594e16b2705ad39fcce426ba696
mosiyw <amirpc.a@yahoo.com>
2024-02-18 20:35:07 +0330

    fixed some fucking errors

--- frontend/src/contexts/context.types.ts
+++ frontend/src/contexts/context.types.ts
@@ -16,4 +16,6 @@ export type ContextType = State & {
   withAuth: (fn: () => void) => void;
   authorize: () => void;
   closeDrawer: () => void;
+  openDrawer: (data?: any) => void; // Add this line
+  setDrawerView: (view: string) => void; // Add this line
 };
