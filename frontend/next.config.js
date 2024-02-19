216234d7ba9e379005227b33dfca0c44865b9cb4
mosiyw <amirpc.a@yahoo.com>
2024-02-19 13:19:20 +0330

    image url fixed

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,8 +2,6 @@
  * @type {import('next').NextConfig}
  */
 const { i18n } = require('./next-i18next.config');
-const { URL } = require('url');
-
 // const withPWA = require('next-pwa');
 const runtimeCaching = require('next-pwa/cache');
 module.exports = {
@@ -15,10 +13,7 @@ module.exports = {
   // },
   i18n,
   images: {
-    domains: [
-      'localhost',
-      new URL(process.env.NEXT_PUBLIC_Images_API_ENDPOINT).hostname,
-    ],
+    domains: ['localhost', 'api.gilasanat.ir', 'gilasanat.ir'],
   },
   ...(process.env.NODE_ENV === 'production' && {
     typescript: {
