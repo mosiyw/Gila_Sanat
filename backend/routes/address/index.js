62830557e79318c6620ac0f613037587863e51eb
mosiyw <amirpc.a@yahoo.com>
2024-02-20 23:17:03 +0330

    add a filter for duplicate zipcode on adress

--- backend/routes/address/index.js
+++ backend/routes/address/index.js
@@ -6,7 +6,7 @@ const authMiddleware = require("../../middlewares/authMiddleware");
 
 router.use(authMiddleware.authenticate);
 
-router.post("/", addressController.addAddress);
+router.post("/add", addressController.addAddress);
 router.delete("/:index", addressController.removeAddress);
 router.get("/", addressController.getAddresses);
 router.get("/test", (req, res) => {
