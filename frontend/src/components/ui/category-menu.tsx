6eea998ac4a7fa087790094bfafcf97959b69466
mosiyw <amirpc.a@yahoo.com>
2024-03-21 17:30:28 +0330

    categories menu update

--- frontend/src/components/ui/category-menu.tsx
+++ frontend/src/components/ui/category-menu.tsx
@@ -5,6 +5,7 @@ import Link from '@components/ui/link';
 import { IoIosArrowBack } from 'react-icons/io';
 import Image from '@components/ui/image';
 import { ROUTES } from '@utils/routes';
+import Grid from '@mui/material/Grid';
 
 function SidebarMenuItem({
   className,
@@ -85,7 +86,7 @@ function SidebarMenuSubItem({
             {second_subcategories.map((subcategory) => (
               <li
                 key={`${subcategory.name}${subcategory._id}`}
-                className="text-xs px-3 py-1 text-brand-muted hover:text-brand border-b border-border-base last:border-b-0 mb-0.5"
+                className="text-xs px-3 py-1 text-brand-muted hover:text-brand mb-0.5"
               >
                 {subcategory.name}
               </li>
@@ -96,42 +97,86 @@ function SidebarMenuSubItem({
     </>
   );
 }
-
 function SidebarMenu({ items, className }: any) {
   const [activeCategory, setActiveCategory] = useState(items[0]); // Set initial active category to the first item
 
   return (
-    <ul
+    <div
       className={cn(
-        'w-64 md:w-72 h-430px bg-brand-light border border-border-base  category-dropdown-menu pt-1.5',
+        'w-[80vw] md:w-[80vw] h-430px bg-brand-light border border-border-base  category-dropdown-menu pt-1.5',
         className
       )}
     >
-      {items?.map((item: any) => (
-        <SidebarMenuItem
-          key={`${item._id}-key-${item.id}`}
-          item={item}
-          setActiveCategory={setActiveCategory}
-        />
-      ))}
-      <div className="absolute top-0 z-10 w-full h-full border  md:block right-full bg-brand-light border-border-base">
-        <ul key="content" className="text-xs py-1.5">
-          {activeCategory.subcategories?.map((subcategory) => {
-            return (
-              <SidebarMenuSubItem
-                key={`${subcategory.name}${subcategory._id}`}
-                item={subcategory}
-                depth={1}
-                className={cn(
-                  'text-sm px-3 py-3 ltr:pr-3 rtl:pl-3 text-brand-muted hover:text-brand border-b border-border-base last:border-b-0 mb-0.5'
-                )}
-                setActiveCategory={setActiveCategory}
-              />
-            );
-          })}
-        </ul>
-      </div>
-    </ul>
+      <Grid container spacing={2}>
+        <Grid item xs={3}>
+          {items?.map((item: any) => (
+            <SidebarMenuItem
+              key={`${item._id}-key-${item.id}`}
+              item={item}
+              setActiveCategory={setActiveCategory}
+            />
+          ))}
+        </Grid>
+        <Grid item xs={9}>
+          <div className="z-10 w-full h-full border  md:block right-full bg-brand-light border-border-base">
+            <Grid container spacing={2}>
+              <Grid item xs={3}>
+                {activeCategory.subcategories?.map((subcategory) => (
+                  <SidebarMenuSubItem
+                    key={`${subcategory.name}${subcategory._id}`}
+                    item={subcategory}
+                    depth={1}
+                    className={cn(
+                      'text-sm px-3 py-3 ltr:pr-3 rtl:pl-3 text-brand-muted hover:text-brand border-b border-border-base last:border-b-0 mb-0.5'
+                    )}
+                    setActiveCategory={setActiveCategory}
+                  />
+                ))}
+              </Grid>
+              <Grid item xs={3}>
+                {activeCategory.subcategories?.map((subcategory) => (
+                  <SidebarMenuSubItem
+                    key={`${subcategory.name}${subcategory._id}`}
+                    item={subcategory}
+                    depth={1}
+                    className={cn(
+                      'text-sm px-3 py-3 ltr:pr-3 rtl:pl-3 text-brand-muted hover:text-brand border-b border-border-base last:border-b-0 mb-0.5'
+                    )}
+                    setActiveCategory={setActiveCategory}
+                  />
+                ))}
+              </Grid>
+              <Grid item xs={3}>
+                {activeCategory.subcategories?.map((subcategory) => (
+                  <SidebarMenuSubItem
+                    key={`${subcategory.name}${subcategory._id}`}
+                    item={subcategory}
+                    depth={1}
+                    className={cn(
+                      'text-sm px-3 py-3 ltr:pr-3 rtl:pl-3 text-brand-muted hover:text-brand border-b border-border-base last:border-b-0 mb-0.5'
+                    )}
+                    setActiveCategory={setActiveCategory}
+                  />
+                ))}
+              </Grid>
+              <Grid item xs={3}>
+                {activeCategory.subcategories?.map((subcategory) => (
+                  <SidebarMenuSubItem
+                    key={`${subcategory.name}${subcategory._id}`}
+                    item={subcategory}
+                    depth={1}
+                    className={cn(
+                      'text-sm px-3 py-3 ltr:pr-3 rtl:pl-3 text-brand-muted hover:text-brand border-b border-border-base last:border-b-0 mb-0.5'
+                    )}
+                    setActiveCategory={setActiveCategory}
+                  />
+                ))}
+              </Grid>
+            </Grid>
+          </div>
+        </Grid>
+      </Grid>
+    </div>
   );
 }
 
