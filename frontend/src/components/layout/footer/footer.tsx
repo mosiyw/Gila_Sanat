9bff491a066fdf5bb5bc44d06c70f69a57aa7e67
mosiyw <amirpc.a@yahoo.com>
2024-03-22 18:27:50 +0330

    deploy error fixed

--- frontend/src/components/layout/footer/footer.tsx
+++ frontend/src/components/layout/footer/footer.tsx
@@ -1,12 +1,12 @@
 import Widgets from '@components/layout/footer/widget/widget';
 import Copyright from '@components/layout/footer/copyright';
 import { footer } from './data';
-const { widgets, payment } = footer;
+const { widgets } = footer;
 
 const Footer: React.FC = () => (
   <footer className="mt-[50px] lg:mt-14 2xl:mt-16" dir="rtl">
     <Widgets widgets={widgets} />
-    <Copyright payment={payment} />
+    <Copyright />
   </footer>
 );
 
